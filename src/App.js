f3c3f31f3f7c193aacae00057e1e2266688927e8
tzur <Tzur.almog@gmail.com>
2021-10-14 15:59:53 -0400

    Added tasks: tutuoial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import Header from "./components/Header";
+import Tasks from './components/Tasks'
+import { useState} from 'react'
+import Task from "./components/Task";
+
 
 function App() {
-  const name = "brad"
+  const [tasks, setTasks] = useState([{
+    text: "OMG",
+    id:0,
+    remider:false,
+    day:"Thursday",
+},{
+    text: "OMG Lol",
+    id:1,
+    remider:false,
+    day:"Friday",
+},{
+    text: "OMG BUG",
+    id:2,
+    remider:true,
+    day:"Saturday",
+},])
+
+// delete task
+const deleteTask = (id) => {
+  // console.log("delete ", id)
+  setTasks( tasks.filter((task) => task.id !== id))
+}
+
 
   return (
     <div className="container">
       {/* <h1>Testing auto resolve</h1>
       <h2> Hello {name}</h2> */}
       <Header title="Room 1"/>
+      { tasks.length > 0 ? <Tasks tasks = {tasks}  onDelete = {deleteTask} /> : "No Tasks to show"}
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = ()=> {
+        console.log("click")
+    }
     return (
-        <header>
+        <header className = 'header'>
             <h1 >{title}</h1>
+            <Button color ='green' text = 'hello' onClick= {onClick}/>
         </header>
     )
 }
